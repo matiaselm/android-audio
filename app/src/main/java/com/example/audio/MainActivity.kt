5ad663cbcdf551f87e9e0133751c047e3925f636
Matias Jalava <matias@elm.fi>
2020-09-20 17:00:27 +0300

    copied the code, need to figure out external files path xml-stuff

--- app/src/main/java/com/example/audio/MainActivity.kt
+++ app/src/main/java/com/example/audio/MainActivity.kt
@@ -1,21 +1,92 @@
 package com.example.audio
 
-import android.media.AudioAttributes
-import android.media.MediaPlayer
+import android.media.*
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
+import java.io.*
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "audio-rec"
+    val recFileName = "testkjs.raw"
+    val storageDir = getExternalFilesDir(Environment.DIRECTORY_MUSIC)
+    lateinit var recFile: File
+    var recRunning: Boolean = false
+
+    fun recordAudio() {
+        try {
+            recFile = File(storageDir.toString() + "/" + recFileName)
+        } catch (e: IOException) {
+            Log.d(TAG, "$e")
+            // Error occrured creating file
+        }
+        try {
+            val outputStream = FileOutputStream(recFile)
+            val bufferedOutputStream = BufferedOutputStream(outputStream)
+            val dataOutputStream = DataOutputStream(bufferedOutputStream)
+
+            val minBufferSize = AudioRecord.getMinBufferSize(44100,
+                    AudioFormat.CHANNEL_OUT_STEREO,
+                    AudioFormat.ENCODING_PCM_16BIT)
+
+            val aFormat = AudioFormat.Builder()
+                    .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                    .setSampleRate(44100)
+                    .setChannelMask(AudioFormat.CHANNEL_OUT_STEREO)
+                    .build()
+
+            val recorder = AudioRecord.Builder()
+                    .setAudioSource(MediaRecorder.AudioSource.MIC)
+                    .setAudioFormat(aFormat)
+                    .setBufferSizeInBytes(minBufferSize)
+                    .build()
+
+            val audioData = ByteArray(minBufferSize)
+
+            recorder.startRecording()
+
+            while (recRunning) {
+                val numofBytes = recorder.read(audioData, 0, minBufferSize)
+                if (numofBytes > 0) {
+                    dataOutputStream.write(audioData)
+                }
+            }
+            recorder.stop()
+            dataOutputStream.close()
+
+        } catch (e: IOException) {
+            Log.d(TAG, "$e")
+            // Some error happened
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        recordAudio()
+
+        button.setOnClickListener {
+            when(recRunning){
+                false -> {
+                    recordingStatus.text = "Recording"
+                    recRunning = true
+                }
+                true -> {
+                    recordingStatus.text = "Start recording"
+                    recRunning = false
+                }
+            }
+        }
+
+        /*
         val sound1: URL = URL("https://freesound.org/data/previews/140/140822_2238878-hq.mp3")
         val sound2: URL = URL("https://freesound.org/data/previews/140/140827_2238878-hq.mp3")
         val sound3: URL = URL("https://freesound.org/data/previews/140/140789_2238878-hq.mp3")
@@ -29,6 +100,7 @@ class MainActivity : AppCompatActivity() {
             st.await()
             tt.await()
         }
+         */
     }
 
     private fun playAudio(track: URL, sel: String) {
